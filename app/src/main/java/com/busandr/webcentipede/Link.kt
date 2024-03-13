1e1c4569d7d5043e95a3c7a7d02222723f1c8767
Arseny <111218794+Busandr@users.noreply.github.com>
2024-03-13 15:12:47 +0300

    Create Link.kt
    data class finally!

---
+++ app/src/main/java/com/busandr/webcentipede/Link.kt
@@ -0,0 +1,22 @@
+package com.busandr.webcentipede
+
+import android.graphics.Bitmap
+import java.util.UUID
+
+data class Link(
+    val id: String = createId(),
+    var name: String = "",
+    val creationTime: Long = System.currentTimeMillis(),
+    var favicon: Bitmap? = null,
+    var isActive: Boolean = true,
+    var keepData: Boolean = false,
+
+    var wifiFrequency: Long = 1,
+    var mobileFrequency: Long = 1
+) {
+    companion object{
+        fun createId(): String{
+            return UUID.randomUUID().toString()
+        }
+    }
+}
