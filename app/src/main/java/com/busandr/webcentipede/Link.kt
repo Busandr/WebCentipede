658e249acd9042d33ab2d701fa026117d2a862d1
Arseny <111218794+Busandr@users.noreply.github.com>
2024-04-26 12:08:46 +0300

    Update Link.kt

--- app/src/main/java/com/busandr/webcentipede/Link.kt
+++ app/src/main/java/com/busandr/webcentipede/Link.kt
@@ -5,7 +5,7 @@ import java.util.UUID
 
 data class Link(
     var name: String,
-    val url: String = "",
+    val url: String,
     val id: String = createId(),
     val creationTime: Long = System.currentTimeMillis(),
     var favicon: Bitmap? = null,
