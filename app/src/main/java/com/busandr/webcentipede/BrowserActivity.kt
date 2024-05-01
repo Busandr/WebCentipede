7de4d6e0de1b88dad3da99e8738f378f20f05280
Arseny <111218794+Busandr@users.noreply.github.com>
2024-05-01 16:39:13 +0300

    Update BrowserActivity.kt

--- app/src/main/java/com/busandr/webcentipede/BrowserActivity.kt
+++ app/src/main/java/com/busandr/webcentipede/BrowserActivity.kt
@@ -17,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class BrowserActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browser)
     }
 }
