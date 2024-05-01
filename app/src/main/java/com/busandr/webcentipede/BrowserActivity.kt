2865eef18ff6313c1c249c3530f123c9002ef9e7
Arseny <111218794+Busandr@users.noreply.github.com>
2024-05-01 16:31:13 +0300

    Create BrowserActivity.kt

---
+++ app/src/main/java/com/busandr/webcentipede/BrowserActivity.kt
@@ -0,0 +1,22 @@
+package com.busandr.webcentipede
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.http.SslError
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.webkit.SslErrorHandler
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class BrowserActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+    }
+}
