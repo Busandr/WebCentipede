8fc3e137d6b912c307e7639244dfe40e5f1ea71e
Arseny <111218794+Busandr@users.noreply.github.com>
2024-07-10 21:07:52 +0300

    Update Link.kt

--- app/src/main/java/com/busandr/webcentipede/Link.kt
+++ app/src/main/java/com/busandr/webcentipede/Link.kt
@@ -4,7 +4,7 @@ import android.graphics.Bitmap
 import java.util.UUID
 
 data class Link(
-    var name: String,
+    var name: String = "google.com",
     val url: String,
     val id: String = createId(),
     val creationTime: Long = System.currentTimeMillis(),
