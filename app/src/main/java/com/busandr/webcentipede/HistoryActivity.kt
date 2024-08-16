fa2a3c904f94ae073e63a3185abc89d0846f52ee
Arseny <111218794+Busandr@users.noreply.github.com>
2024-08-16 17:30:09 +0300

    Create HistoryActivity.kt

---
+++ app/src/main/java/com/busandr/webcentipede/HistoryActivity.kt
@@ -0,0 +1,8 @@
+package com.busandr.webcentipede
+
+class HistoryActivity : FragmentActivity(){
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_history)
+  }
+}
