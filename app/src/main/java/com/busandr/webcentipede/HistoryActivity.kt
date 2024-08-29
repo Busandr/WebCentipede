75f1e983f28ecae559af0f43f17d2bd60bcef349
Arseny <111218794+Busandr@users.noreply.github.com>
2024-08-29 22:16:55 +0300

    Update HistoryActivity.kt

--- app/src/main/java/com/busandr/webcentipede/HistoryActivity.kt
+++ app/src/main/java/com/busandr/webcentipede/HistoryActivity.kt
@@ -4,5 +4,8 @@ class HistoryActivity : FragmentActivity(){
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_history)
+
+    val toolbarHistory = findViewById<Toolbar>(R.id.toolbar_history)
+    
   }
 }
