a03413497278cdeead1256809eca4779a0f0fc55
Arseny <111218794+Busandr@users.noreply.github.com>
2024-09-11 15:25:02 +0300

    Update HistoryActivity.kt
    dbHelper creation

--- app/src/main/java/com/busandr/webcentipede/HistoryActivity.kt
+++ app/src/main/java/com/busandr/webcentipede/HistoryActivity.kt
@@ -6,10 +6,14 @@ class HistoryActivity : FragmentActivity(){
     setContentView(R.layout.activity_history)
 
     val toolbarHistory = findViewById<Toolbar>(R.id.toolbar_history)
-    
+    val dbHelper = DatabaseHelper.DatabaseManager.getInstance(this)
+
+    var linkInstance: Link = dbHelper?.readLinkById(intent.getStringExtra("link_id").toString())!!
     var textHistory: TextView = findViewById(R.id.text_history)
     val buttonPrevVersion: Button = findViewById(R.id.prev_version)
     val buttonNextVersion: Button = findViewById(R.id.next_version)
+
+    textHistory.text = linkInstance.content
     
   }
 }
